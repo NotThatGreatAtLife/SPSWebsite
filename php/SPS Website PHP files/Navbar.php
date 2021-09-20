a2686a73219e6baa78f47e269275b954c8820af0
JezenVolk <dylanherman@ucsb.edu>
2021-09-20 09:30:37 -0700

    Update 9/20(organized everything)

---
+++ php/SPS Website PHP files/Navbar.php
@@ -0,0 +1,73 @@
+    
+    <nav class="navbar navbar-light sticky-top navbar-expand-lg" style="background-color:#DCE1E5; overflow:unset">
+    <div class="container">
+        <div class="row">
+        <div class="col-md-12">
+        <div class="navbar-nav" >
+            <ul>
+              <li class="nav-item " style="list-style-type:none">
+                <a class="nav-link " style="color:black" href="/SPSHome.html">
+                  Home
+                </a>
+              </li>
+              </ul>
+
+            <ul>
+            <li class="nav-item dropdown" style="list-style-type:none">
+              <a class="nav-link dropdown-toggle" style="color:black" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                About
+              </a>
+              <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                <li><a class="dropdown-item" href="Mission Statement.html">Mission Statement</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="Officer Gallery.html">Officer Gallery</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="FAQs.html">FAQs</a></li>
+
+
+              </ul>
+            </li>
+            </ul>
+
+            <ul>
+              <li class="nav-item dropdown" style="list-style-type:none">
+                <a class="nav-link dropdown-toggle" style="color:black" href="Programs.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                  Programs
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="JournalClub.html">Journal club</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="MentorshipProgram.html">Mentorship Program</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="# ">Competitions</a></li>
+                  
+                </ul>
+              </li>
+              </ul>
+
+            <div class="nav-item nav-link"><a href="SPSORG.html">SPS Organization</a></div>
+            <div class="nav-item nav-link"><a href ="Events.html">Events</a></div>
+            <ul>
+              <li class="nav-item dropdown" style="list-style-type:none">
+                <a class="nav-link dropdown-toggle" style="color:black" href="Resources.html" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                  Resources
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                  <li><a class="dropdown-item" href="#">Basic</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Other Clubs</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Research Help</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Helpful Links</a></li>
+                  <li><hr class="dropdown-divider"></li>
+                  <li><a class="dropdown-item" href="#">Cute Animals</a></li>
+                </ul>
+              </li>
+              </ul>
+            <div class="nav-item nav-link"><a href ="Contact.html">Contact</a></div>
+        </div>
+        </div>
+        </div>
+    </div>
+</nav>
